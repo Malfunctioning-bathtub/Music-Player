c2be5c1c9941a545291f50ab0d5405f495110051
Malfunctioning Bathtub <jacobheller638@gmail.com>
2025-02-06 18:37:22 +0000

    Can now play and pause properly from GUI

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ init()
 
 root = tk.Tk()
 
-
-
 music.load("Music-player/piddling-simple-drums-full-house_126bpm.wav")
-music.play()
+
+def onPlayButtonPressed():
+    music.play()
+
+def onPauseButtonPressed():
+    music.pause()
+
+
+
+playButton = tk.Button(root, text='Play', width=25, command=onPlayButtonPressed)
+playButton.pack()
+
+pauseButton = tk.Button(root, text='Pause', width=25, command=onPauseButtonPressed)
+pauseButton.pack()
+
 
 
 root.mainloop()
