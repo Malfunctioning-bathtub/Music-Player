e74368283935b4bd18fc1bbbc6274dc38e750930
Malfunctioning Bathtub <jacobheller638@gmail.com>
2025-02-06 18:27:45 +0000

    Switched to pygame audio and made a tkinter window

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # import required modules
-from tkinter import *
+import tkinter as tk
 import time as tm
-from pydub import *
-from pydub.playback import _play_with_simpleaudio
+from pygame.mixer import *
 
-song = AudioSegment.from_file("Music-player/piddling-simple-drums-full-house_126bpm.wav")
-song = song.set_sample_width(2)
+init()
 
-playback = _play_with_simpleaudio(song)
+root = tk.Tk()
 
-# end playback after 3 seconds
-tm.sleep(3)
-playback.stop()
+
+
+music.load("Music-player/piddling-simple-drums-full-house_126bpm.wav")
+music.play()
+
+
+root.mainloop()
